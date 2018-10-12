ae9b8e340928a1bbf826aeeb458d5ce001a2f0c2
Ben Kiggen <benkiggen@gmail.com>
2018-10-12 15:45:28 -0700

    configure doctor information into constructor, successfully display on page

---
+++ src/Doctor.js
@@ -0,0 +1,33 @@
+export default class Doctor {
+  constructor(docReturn) {
+    this.accepts = accepts(docReturn);
+    this.docName = `${docReturn.profile.first_name} ${docReturn.profile.last_name}`;
+    this.pictureURL = docReturn.profile.image_url;
+    this.docTitle = docReturn.profile.title;
+    this.languages = languages(docReturn);
+    this.number = docReturn.practices[0].phones[0].number
+    this.address = `${docReturn.practices[0].visit_address.street},  ${docReturn.practices[0].visit_address.city}, ${docReturn.practices[0].visit_address.state}, ${docReturn.practices[0].visit_address.zip}`;
+  }
+}
+
+
+function accepts(docReturn) {
+
+  if (docReturn.practices[0].accepts_new_patients === true){
+    docReturn = "yes";
+  } else {
+    docReturn = "not currently";
+  }
+  return docReturn;
+}
+
+function languages(array) {
+  console.log(array);
+  let languages = array.practices[0].languages;
+  let newString = [];
+  for(let i=0; i < languages.length; i++) {
+    newString.push(languages[i].name);
+    return newString;
+  }
+
+}
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './scss/styles.scss';
 import $ from 'jquery';
 import DocSearch from './DocSearch.js';
-import ReturnList from './ReturnList.js';
+import Doctor from './Doctor.js';
 import DocProfile from './DocProfile';
 
 $(document).ready(function(){
@@ -23,19 +23,17 @@ $(document).ready(function(){
   })
   function displayData(docReturn) {
     for(let i=0; i < docReturn.length; i++){
-      console.log(docReturn[0]);
-      $(".displayData").append(`<li class='doctorEntry'><div class="card" width:200px><div class="card-body"><img src="${docReturn[i].profile.image_url}" alt="photo of ${docReturn[i].profile.first_name} ${docReturn[i].profile.last_name}" width:180px>
-      <h5 class="card-title">${docReturn[i].profile.first_name} ${docReturn[i].profile.last_name}</h5>
-      <h6 class="card-subtitle mb-2 text-muted">${docReturn[i].profile.title}</h6><ul>
-      <li>Languages: ${docReturn[i].practices[0].languages[0].name}</li><li>Address: ${docReturn[i].practices[0].visit_address.street}, ${docReturn[i].practices[0].visit_address.city}, ${docReturn[i].practices[0].visit_address.state}, ${docReturn[i].practices[0].visit_address.zip}</li>
-      </ul></div></div></li>`)
+      let doctor = new Doctor(docReturn[i]);
+      $(".displayData").append(`<li class='doctorEntry'><div class="card" width:200px><div class="card-body"><img src="${doctor.pictureURL}" alt="photo of ${doctor.docName}" width:180px>
+      <h5 class="card-title">${doctor.docName}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">${doctor.docTitle}</h6><ul>` +
+      `<li>Languages: ${doctor.languages}</li><li>Address: ${doctor.address}</li>` + `<li>Phone Numbers: ${doctor.number}</li><li>Accepting New Patients: ${doctor.accepts}</li></ul></div></div></li>`);
+
     }
   }
 })
 
 
-      // <li>Practices: ${docReturn[i].practices[0].phones[0].number}</li>
-      // <li>Accepting New Patients: ${docReturn.practices[0].accepts_new_patients}</li>
-
+//
 //
 // <li>Website:<a href="${docReturn[i].practices[0].website}" class="card-link">${docReturn[i].practices[0].website}</a></li>
