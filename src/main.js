79123106b609f6ddb7aacf6a848b3b5e16bce602
Ben Kiggen <benkiggen@gmail.com>
2018-10-12 14:12:44 -0700

    add jquery, adjust api

--- src/main.js
+++ src/main.js
@@ -23,16 +23,19 @@ $(document).ready(function(){
   })
   function displayData(docReturn) {
     for(let i=0; i < docReturn.length; i++){
-      $(".displayData").append(`<li class='doctorEntry'><div class="card" width: 18rem><div class="card-body">` + `<img src="${docReturn[i].profile.image_url}" alt="photo of ${docReturn[i].profile.first_name} ${docReturn[i].profile.last_name}" style="width: 15rem"> +
+      console.log(docReturn[0]);
+      $(".displayData").append(`<li class='doctorEntry'><div class="card" width:200px><div class="card-body"><img src="${docReturn[i].profile.image_url}" alt="photo of ${docReturn[i].profile.first_name} ${docReturn[i].profile.last_name}" width:180px>
       <h5 class="card-title">${docReturn[i].profile.first_name} ${docReturn[i].profile.last_name}</h5>
-      <h6 class="card-subtitle mb-2 text-muted">${docReturn[i].profile.title}</h6>
-      <ul><li>Website:<a href="${docReturn[i].website}" class="card-link">${docReturn[i].website}</a>${docReturn[i].website}</li></ul></div></div></li>`)
+      <h6 class="card-subtitle mb-2 text-muted">${docReturn[i].profile.title}</h6><ul>
+      <li>Languages: ${docReturn[i].practices[0].languages[0].name}</li><li>Address: ${docReturn[i].practices[0].visit_address.street}, ${docReturn[i].practices[0].visit_address.city}, ${docReturn[i].practices[0].visit_address.state}, ${docReturn[i].practices[0].visit_address.zip}</li>
+      </ul></div></div></li>`)
     }
   }
 })
 
+
+      // <li>Practices: ${docReturn[i].practices[0].phones[0].number}</li>
+      // <li>Accepting New Patients: ${docReturn.practices[0].accepts_new_patients}</li>
+
 //
-// <li>Languages: ${docReturn[i].practices[0].languages}</li>
-// <li>Practices: ${docReturn[i].practices[0].phones[0]}</li>
-// <li>Address: ${docReturn[i].practices[0].visit_address}</li>
-// <li>Accepting New Patients: ${docReturn.practices[0].accepts_new_patients}</li>
+// <li>Website:<a href="${docReturn[i].practices[0].website}" class="card-link">${docReturn[i].practices[0].website}</a></li>
