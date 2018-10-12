0c33a227560218060af312e779dca59352d4bac5
Ben Kiggen <benkiggen@gmail.com>
2018-10-12 10:17:13 -0700

    add initial work on doctor array constructor

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './scss/styles.scss';
 import $ from 'jquery';
 import DocSearch from './DocSearch.js';
+import ReturnList from './ReturnList.js';
 
 $(document).ready(function(){
   let userIssue = "heart attack";
-  let userDocName = "Beckerman";
+  let userDocName = "";
   let userLattitude = 45.5122;
   let userLongitude = -122.6587;
   let userRange = 25;
@@ -16,6 +17,6 @@ $(document).ready(function(){
   let promise = docQuery.docQuery(userIssue, userDocName, userLattitude, userLongitude, userRange);
   promise.then(function(response){
     let body = JSON.parse(response);
-    console.log(body);
+    let docList = new ReturnList(body);
   })
 })
